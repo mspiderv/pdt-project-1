5131af7e159dea3c0c9ed6975ce6cbb8e2b383e0
Matej Vitaz <mspiderv@gmail.com>
2016-10-05 21:46:50 +0200

    SemanticUI Initialized

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,44 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
 
-        <link rel="stylesheet" href="{{asset('css/vendor/normalize.min.css')}}">
-        <link rel="stylesheet" href="{{asset('https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css')}}" rel='stylesheet'>
-        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="stylesheet" href="{!! asset('https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css') !!}" rel='stylesheet'>
+        <link rel="stylesheet" href="{!! asset('css/semantic.min.css') !!}">
+        <link rel="stylesheet" href="{!! asset('css/main.css') !!}">
 
         <!--[if lt IE 9]>
-            <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-            <script>window.html5 || document.write('<script src="/js/vendor/html5shiv.js"><\/script>')</script>
+            <script src="{!! asset('js/vendor/html5-3.6-respond-1.4.2.min.js') !!}"></script>
         <![endif]-->
     </head>
     <body>
-        <div id="map"></div>
-        <script src='{{asset('js/vendor/jquery-1.11.2.min.js')}}'></script>
-        <script src='{{asset('https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js')}}'></script>
-        <script src="{{asset('js/main.js')}}"></script>
+        
+        <div class="ui grid maxheight">
+            <div class="three wide column" id="sidebar">
+                
+                <div class="ui vertical fluid tabular menu">
+                    <a class="active item">
+                        Bio
+                    </a>
+                    <a class="item">
+                        Pics
+                    </a>
+                    <a class="item">
+                        Companies
+                    </a>
+                    <a class="item">
+                        Links
+                    </a>
+                </div>
+                
+            </div>
+            <div class="thirteen wide column maxheight">
+                <div class="maxheight" id="map"></div>
+            </div>
+        </div>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="{!! asset('js/vendor/jquery-1.11.2.min.js') !!}"><\/script>')</script>
+        <script src='{!! asset('https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js') !!}'></script>
+        <script src="{!! asset('js/vendor/semantic.min.js') !!}"></script>
+        <script src="{!! asset('js/main.js') !!}"></script>
     </body>
 </html>
