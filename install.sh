bc4e131a1bd8183af396559572e8c12b3fa80f06
Matej Vitaz <mspiderv@gmail.com>
2016-11-11 19:12:10 +0100

    Example of .env file updated

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo chmod -R 777 storage/
 sudo chmod -R 777 bootstrap/cache/
 cp .env.example .env
+sudo chmod 777 .env
 composer install
 php artisan key:generate
 echo "Please configure .env file, then import map data."
